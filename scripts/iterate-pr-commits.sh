8851c2914a17b67b743b3eb6e6a880dc9bce35e3
Dave Kerr <dwmkerr@gmail.com>
2025-03-05 11:51:05 +0000

    testing action

--- scripts/iterate-pr-commits.sh
+++ scripts/iterate-pr-commits.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+# Bin it if we have any errors.
+set -e
+set -o pipefail
+
 # Note that in a GitHub action you can get the base ref with:
 #   ${{ github.event.pull_request.base.ref }}
 # If we are not provided with a base, assume main.
