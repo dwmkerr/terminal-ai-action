e75b301edba4a36d72abba2e30bc6fdcc7b62402
Dave Kerr <dwmkerr@gmail.com>
2025-03-05 11:47:08 +0000

    feat(pr): iterate over commits script

--- scripts/iterate-pr-commits.sh
+++ scripts/iterate-pr-commits.sh
@@ -1,10 +1,11 @@
 # Note that in a GitHub action you can get the base ref with:
-# ${{ github.event.pull_request.base.ref }}
-base_ref="main"
-echo "Base Ref: ${base_ref}"
+#   ${{ github.event.pull_request.base.ref }}
+# If we are not provided with a base, assume main.
+base_ref="${2:main}"
+echo "Comparing to: ${base_ref}"
 
 # Fetch the base so that we know its commits.
-git fetch origin ${base_ref}
+git fetch --quiet origin ${base_ref}
 
 # Get the list of commits between main and the current branch
 commits=$(git log main..HEAD --pretty=format:"%H")
@@ -12,6 +13,7 @@ commits=$(git log main..HEAD --pretty=format:"%H")
 # Loop through each commit
 while IFS= read -r commit; do
   echo "Commit: $commit"
-  # git diff "$commit^" "$commit"  # Show the diff for the commit
+  git log -1 "$commit"  # Show the diff for the commit
+  git diff "$commit^" "$commit"  # Show the diff for the commit
   echo
 done <<< "$commits"
